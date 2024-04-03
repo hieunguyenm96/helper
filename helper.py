285f2fc789378d6c1e5bb365328785129ca01120
Hieu Nguyen Minh <hieunguyenm.dev@gmail.com>
2024-04-03 13:55:40 +0700

    init class kamailio

--- helper.py
+++ helper.py
@@ -14,3 +14,14 @@ def test():
 class kamailio:
     def __init__(self):
         KSR.info('===== kamailio.__init__\n')
+    
+    # executed when kamailio child processes are initialized
+    def child_init(self, rank):
+        KSR.info('===== kamailio.child_init(%d)\n' % rank)
+        return 0;
+
+    # SIP request routing
+    # -- equivalent of request_route{}
+    def ksr_request_route(self, msg):
+        KSR.info("===== request - from kamailio python script\n")
+        KSR.dbg("method " + KSR.pv.get("$rm") + " r-uri " + KSR.pv.get("$ru"))
