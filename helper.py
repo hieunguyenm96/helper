e66a9b01bbe82d4bb573365d08c8debc8d4c0a79
Hieu Nguyen Minh <hieunguyenm.dev@gmail.com>
2024-04-03 13:44:21 +0700

    create fnc mod_init

--- helper.py
+++ helper.py
@@ -3,6 +3,14 @@
 import sys
 import KSR as KSR # type: ignore
 
+def mod_init():
+    KSR.info("===== from Python mod init\n")
+    return kamailio()
+
 def test():
     KSR.info("===== from helper.py =====\n")
     return True
+
+class kamailio:
+    def __init__(self):
+        KSR.info('===== kamailio.__init__\n')
